c80e851e1152e2935fa6a90a23bb7d46b72f96f7
Mansi Bhayade <mansibhayade0804@gmail.com>
2023-09-28 14:09:43 +0530

    Updated favoriteActivity

--- app/src/main/java/dev/ukanth/iconmgr/FavoriteAcitvity.java
+++ app/src/main/java/dev/ukanth/iconmgr/FavoriteAcitvity.java
@@ -1,17 +1,25 @@
 package dev.ukanth.iconmgr;
-import android.app.Activity;
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Typeface;
 import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
-import android.util.DisplayMetrics;
+import android.os.Environment;
+import android.util.Log;
 import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.GridLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import com.afollestad.materialdialogs.MaterialDialog;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -19,19 +27,18 @@ import java.util.Map;
 import dev.ukanth.iconmgr.dao.FavDao;
 
 
-public class FavoriteAcitvity extends Activity {
-
-    private GridLayout gridLayout;
-
-    private LinearLayout.LayoutParams params;
-
+public class FavoriteAcitvity  extends AppCompatActivity {
     FavDao favDao = App.getInstance().getFavDao();
+    private Context mContext;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.favorite);
+        mContext = FavoriteAcitvity.this;
+        setupActionBar();
         LinearLayout iconPackContainer = findViewById(R.id.icon_pack_container);
         List<String> iconName = favDao.geticonName();
 
@@ -54,7 +61,6 @@ public class FavoriteAcitvity extends Activity {
             // Create a TextView for the current icon name
             TextView textView = new TextView(this);
             textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);  // Text size
-            textView.setTypeface(null, Typeface.BOLD);
             textView.setText(icon);
             // Add the TextView to the icon pack container
             iconPackContainer.addView(textView);
@@ -73,11 +79,70 @@ public class FavoriteAcitvity extends Activity {
             for (byte[] iconImageData : iconImages) {
                 Bitmap bitmap = BitmapFactory.decodeByteArray(iconImageData, 0, iconImageData.length);
 
+                String icontitle = favDao.getIcontitleForIconImageData(iconImageData);
+
                 ImageView image = new ImageView(getApplicationContext());
                 image.setPadding(15, 15, 15, 15);
                 image.setScaleType(ImageView.ScaleType.FIT_CENTER);
                 image.setImageDrawable(new BitmapDrawable(getResources(), bitmap));
 
+                image.setOnClickListener(v -> {
+                    View dialogView = LayoutInflater.from(mContext).inflate(R.layout.favorite_dialog, null);
+                    ImageView saveImageView = dialogView.findViewById(R.id.saveImageView);
+                    ImageView closeImageView = dialogView.findViewById(R.id.closeImageView);
+                    ImageView removeImageView = dialogView.findViewById(R.id.removeImageView);
+                    TextView titletextView = dialogView.findViewById(R.id.icon_title);
+
+                    MaterialDialog dialog = new MaterialDialog.Builder(mContext)
+                            .customView(dialogView, false)
+                            .show();
+
+                    titletextView.setText("Action for"+" "+ icontitle );
+
+                    saveImageView.setOnClickListener(view -> {
+                        File mediaDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+                        if (mediaDir == null) {
+                            Log.e("MICO", "External storage not available");
+                            return;
+                        }
+                        File myDir = new File(mediaDir, "micopacks/general/");
+                        if (!myDir.exists() && !myDir.mkdirs()) {
+                            Log.e("MICO", "Failed to create directory: " + myDir.getAbsolutePath());
+                            return;
+                        }
+                        String fname =  icontitle + ".png";
+                        File file = new File(myDir, fname);
+                        if (file.exists()) {
+                            boolean deleted = file.delete();
+                            if (deleted) {
+                                Toast.makeText(getApplicationContext(), "Image deleted successfully", Toast.LENGTH_SHORT).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Failed to delete image", Toast.LENGTH_SHORT).show();
+                            }
+                            return;
+                        }
+                                try {
+                                    FileOutputStream out = new FileOutputStream(file);
+                                    bitmap.compress(Bitmap.CompressFormat.PNG, 85, out);
+                                    out.flush();
+                                    out.close();
+                                    Toast.makeText(getApplicationContext(), "Icon saved successfully: " + file.getAbsolutePath(), Toast.LENGTH_SHORT).show();
+                                } catch (Exception e) {
+                                    Log.e("MICO", e.getMessage(), e);
+                                }
+                });
+
+                    removeImageView.setOnClickListener(view -> {
+                        favDao.deleteIcon(iconImageData);
+                        Toast.makeText(mContext, "Removed from favorites: " , Toast.LENGTH_SHORT).show();
+
+                    });
+
+                    closeImageView.setOnClickListener(view -> {
+                        dialog.dismiss();
+                    });
+
+                });
                 // Add the ImageView to the GridLayout
                 iconGridLayout.addView(image);
             }
@@ -85,6 +150,11 @@ public class FavoriteAcitvity extends Activity {
             iconPackContainer.addView(iconGridLayout);
 
         }
-
     }
-}
+    private void setupActionBar() {
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+    }
--- app/src/main/java/dev/ukanth/iconmgr/dao/FavDao.java
+++ app/src/main/java/dev/ukanth/iconmgr/dao/FavDao.java
@@ -19,8 +19,14 @@ public interface FavDao {
     @Update
     void updateFavorite(Favorite favorite);
 
+    @Query("DELETE FROM favorite WHERE iconImageData = :imageData")
+    void deleteIcon(byte[] imageData);
+
     @Query("DELETE FROM Favorite WHERE iconPkg = :packageName AND Icontitle = :iconTitle AND iconName = :iconName")
     void deleteFavorite(String packageName, String iconTitle, String iconName);
+
+    @Query("SELECT Icontitle FROM Favorite WHERE iconImageData = :iconImageData LIMIT 1")
+    String getIcontitleForIconImageData(byte[] iconImageData);
 
     @Query("SELECT iconImageData FROM Favorite WHERE iconName = :iconName ")
     List<byte[]> getIconImageData(String iconName);
